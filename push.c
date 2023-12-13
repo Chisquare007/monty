b0e9a59e680e85772228c4baf812f21879853048
chioma victor <chisquarevic@gmail.com>
2023-12-13 10:23:58 -0800

    monty

--- push.c
+++ push.c
@@ -1,43 +1,34 @@
 #include "monty.h"
 /**
- * _push - add node to the stack
+ * f_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void _push(stack_t **head, unsigned int counter)
+void f_push(stack_t **head, unsigned int counter)
 {
-	int n, i = 0, flag = 0;
+	int n, j = 0, flag = 0;
 
 	if (bus.arg)
 	{
 		if (bus.arg[0] == '-')
-			i++;
-		while (bus.arg[j] != '\0')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
 		{
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-			{
-				flag = 1;
-			}
-			i++;
-		}
+				flag = 1; }
 		if (flag == 1)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", counter);
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE);
-		}
-	}
+			exit(EXIT_FAILURE); }}
 	else
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
+		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		addnode(head, n);
